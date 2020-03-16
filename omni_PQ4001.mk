1664512723eacc50ae2dedf4f7f4bd55f5f12bce
thinhx2 <nthinh1102@gmail.com>
2020-03-16 14:15:45 +0700

    bring up

---
+++ omni_PQ4001.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit device configuration
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Charger
+#PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Time Zone data for recovery
+#PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := PQ4001
+PRODUCT_NAME := omni_PQ4001
+PRODUCT_BRAND := Vsmart
+PRODUCT_MODEL := Vsmart Joy 1
+PRODUCT_MANUFACTURER := Vsmart
+PRODUCT_RELEASE_NAME := Vsmart Joy 1
